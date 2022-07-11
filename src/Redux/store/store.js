102273b6fd9b5c84fcc2b6b8f695313906704fc0
Kelly Camayo <Kelly Camayo>
2022-07-11 09:44:53 -0500

    register async

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,4 +1,19 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { userReducers } from "../reducers/userReducers";
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const reducers = combineReducers({
+    
+    regisUserStore: userReducers
+})
+
+export const store = createStore(
+    reducers,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
 
 
-
-// const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from './Routes/AppRoutes';
+import { Provider } from 'react-redux';
+import { store } from './Redux/store/store';
 import "./Styles/Style.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <AppRoutes />
-  </React.StrictMode>
+  </Provider>
 );
 
