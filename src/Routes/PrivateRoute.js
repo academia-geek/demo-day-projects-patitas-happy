61bb69ef38264d3d01ed2207a14b8fa0503885c5
WhitneySt <whitneystena418@gmail.com>
2022-07-12 12:01:46 -0500

    Agregando implementación de rutas públicas y privadas

--- src/Routes/PrivateRoute.js
+++ src/Routes/PrivateRoute.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from 'react'
+import { Navigate } from 'react-router-dom'
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const PrivateRouters = ({ isAutentication, children }) => {
+    return isAutentication ? children : <Navigate to="/login" />
+}
 
-export default PrivateRoute;
+export default PrivateRouters
