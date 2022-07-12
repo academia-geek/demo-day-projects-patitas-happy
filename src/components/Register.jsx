adf817ddc159912056e455f17c193c0326cf6840
WhitneySt <whitneystena418@gmail.com>
2022-07-12 10:15:02 -0500

    Ajustando implementación de login y register con email y contraseña

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,74 @@
 // import { TextField } from '@mui/material';
 import React from 'react';
-import {  Div1, Div2, TitleR } from '../Styles/Styles';
+import { Div1, Div2, TitleR } from '../Styles/Styles';
 import fb from "../img/fb.png"
 import go from "../img/go.png"
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup'
-import { useDispatch } from 'react-redux';
-import { registerUserAsync } from '../Redux/actions/actionsUser';
+import { useDispatch, useSelector } from 'react-redux';
+import { registerUserAsync } from '../Redux/actions/actionsRegister';
+import Swal from "sweetalert2";
+import { useNavigate } from 'react-router-dom';
 
 
 const SignupSchema = Yup.object().shape({
-    nombre: Yup.string().min(2, 'Nombre muy corto').max(20, 'Nombre muy Largo').required("Este nombre es requerido"),
-    email: Yup.string().email('debe ser del tipo ana@ana.com').required("Este email se requiere"),
+    fullname: Yup.string().min(2, 'Nombre muy corto').max(20, 'Nombre muy Largo').required("Este nombre es requerido"),
+    email: Yup.string().email('debe ser del tipo ana@ana.com').required("Este email es requerido"),
     fecha: Yup.date().required("Este campo es requiere"),
-    telefono: Yup.string().min(2,'muy corto').max(12, 'Telefono muy largo').required("Este campo se requiere"),
-    pass1: Yup.string().min(6, 'Password muy corto').max(20, 'Password muy Largo').required("este campo es obligatorio").oneOf([Yup.ref('pass2')], 'Los Password No coinciden'),
-    pass2: Yup.string().min(6, 'Password muy corto').max(20, 'Password muy Largo').required("este campo es obligatorio").oneOf([Yup.ref('pass1')], 'Los Password No coinciden')
+    phoneNumber: Yup.string().min(2, 'muy corto').max(12, 'Telefono muy largo').required("Este campo es requerido"),
+    password: Yup.string().min(6, 'Password muy corto').max(20, 'Password muy Largo').required("este campo es obligatorio").oneOf([Yup.ref('pass2')], 'Los Password No coinciden'),
+    pass2: Yup.string().min(6, 'Password muy corto').max(20, 'Password muy Largo').required("este campo es obligatorio").oneOf([Yup.ref('password')], 'Los Password No coinciden')
 })
 
 const Register = () => {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { error: registerError } = useSelector(store => store.regisUserStore);
+
+
+    //--Función para cantidad de caracteres digitados para número celular
+    const validatePhoneNumber = (str, validStringLength) => {
+        if (!str) return false;
+        const value = str.replace(/\D/g, '');
+        const stringLenght = value.length;
+        return { isValid: stringLenght === validStringLength, value };
+    }
+
+    const onSubmit = (values) => {
+        console.log(values);
+        const { fullname, email, fecha, phoneNumber, password } = values;
+
+        const { isValid, value: validPhoneNumber } = validatePhoneNumber(phoneNumber, 10);
+        if (!isValid) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Por favor verifique el numero teléfonico'
+            });
+
+            return;
+        }
+
+        dispatch(registerUserAsync(fullname, email, fecha, password, validPhoneNumber));
+    }
+
+    if (registerError) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Ha ocurrido un error, por favor intente más tardes.'
+        });
+    } else {
+        if (registerError === false) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Congratulations!',
+                text: 'Se ha creado exitosamente su cuenta!'
+            }).then(() => {
+                navigate("/");
+            });
+        }
+    }
 
     return (
         <Div1>
@@ -30,26 +79,23 @@ const Register = () => {
 
                     initialValues={
                         {
-                            nombre: '',
+                            fullname: '',
                             email: '',
                             fecha: '',
-                            telefono: '',
-                            pass1: '',
+                            phoneNumber: '',
+                            password: '',
                             pass2: ''
                         }
                     }
                     validationSchema={SignupSchema}
-                    onSubmit={values => {
-                        console.log(values.nombre, values.email, values.fecha, values.telefono, values.pass1)
-                        dispatch(registerUserAsync(values.nombre, values.email, values.fecha, values.telefono, values.pass1))
-                    }}
+                    onSubmit={onSubmit}
                 >
                     {({ errors, touched }) => (
 
                         <Form style={{ display: 'flex', flexDirection: 'column' }}>
-                            <Field type="texto" placeholder="Nombre" name="nombre" style={{ margin: "2%" }} />
-                            {errors.nombre && touched.nombre ?
-                                (<div>{errors.nombre}</div>) : null}
+                            <Field type="texto" placeholder="Nombre" name="fullname" style={{ margin: "2%" }} />
+                            {errors.fullname && touched.fullname ?
+                                (<div>{errors.fullname}</div>) : null}
 
                             <Field type="email" placeholder="Email" name="email" style={{ margin: "2%" }} />
                             {errors.email && touched.email ?
@@ -59,17 +105,17 @@ const Register = () => {
                             {errors.fecha && touched.fecha ?
                                 (<div>{errors.fecha}</div>) : null}
 
-                            <Field type="texto" placeholder="Telefono/Celular" name="telefono" style={{ margin: "2%" }} />
-                            {errors.telefono && touched.telefono ?
-                                (<div>{errors.telefono}</div>) : null}
+                            <Field type="texto" placeholder="Telefono/Celular" name="phoneNumber" style={{ margin: "2%" }} />
+                            {errors.phoneNumber && touched.phoneNumber ?
+                                (<div>{errors.phoneNumber}</div>) : null}
 
-                            <Field type="password" placeholder="Password 1" name="pass1" style={{ margin: "2%" }} />
-                            {errors.pass1 && touched.pass1 ?
-                                (<div>{errors.pass1}</div>) : null}
+                            <Field type="password" placeholder="Password 1" name="password" style={{ margin: "2%" }} />
+                            {errors.password && touched.password ?
+                                (<div>{errors.password}</div>) : null}
 
                             <Field type="password" placeholder="Password 2" name="pass2" style={{ margin: "2%" }} />
                             {errors.pass2 && touched.pass2 ?
-                                (<div>{errors.pass1}</div>) : null}
+                                (<div>{errors.password}</div>) : null}
 
                             <button type="submit" style={{ margin: "2%" }}>
                                 Registrar
