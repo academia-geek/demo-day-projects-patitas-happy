0fe73cbf1d478f8d3cdb26a98ada90b1d674e432
Kelly Liliana Camayo Pulido <62678385+kellycamayo08@users.noreply.github.com>
2022-07-12 12:54:41 -0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ import "../Styles/LoginStyle.css";
 import facebookIcon from "../assets/fb.png";
 import googleIcon from "../assets/go.png";
 
-import { actionAuthenticatedSync, actionLoginAsync, actionLoginSync } from "../Redux/actions/actionsLogin";
+import { actionAuthenticatedSync, actionLoginAsync, loginGoogle, loginFacebook, actionLoginSync } from "../Redux/actions/actionsLogin";
 import { Link, useNavigate } from "react-router-dom";
 
 
