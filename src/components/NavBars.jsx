5ae85587aa5f1f8e4ea438043d8bc1ecbb7cc45b
Geiler Prada <geilerprada7@gmail.com>
2022-07-15 15:15:00 -0500

    primer formulario correccion de errores

--- src/components/NavBars.jsx
+++ src/components/NavBars.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import logo from '../assets/logo.png'
-import { ButtonLogout, ImgProfile } from '../Styles/StyleNav';
-import { useDispatch, useSelector } from 'react-redux';
-import { actionLogoutAsync } from '../Redux/actions/actionsRegister';
-import { Link } from 'react-router-dom'
-import { authentication } from '../Firebase/firebaseConfig';
-
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import { ButtonLogout, ImgProfile } from "../Styles/StyleNav";
+import { useDispatch, useSelector } from "react-redux";
+import { actionLogoutAsync } from "../Redux/actions/actionsRegister";
+import { Link } from "react-router-dom";
+import { authentication } from "../Firebase/firebaseConfig";
 
 const NavBars = () => {
   const dispatch = useDispatch();
 
+  const onClick = () => {
+    dispatch(actionLogoutAsync());
+    localStorage.clear();
+    document.location.reload(true);
+  };
+
   const user = useSelector(store => store.loginStore);
+  console.log(user);
+
+  const ver = e => {};
   return (
     <div>
       <Navbar className="m-0" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">
-            <img
-              src={logo}
-              width="124"
-              height="124"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+          <Navbar.Brand>
+            <Link to="/home">
+              <img
+                src={logo}
+                width="124"
+                height="124"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+              />
+            </Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse
@@ -31,10 +41,10 @@ const NavBars = () => {
             className="justify-content-end"
           >
             <Nav className="justify-content-end">
-              <Nav.Link className="mx-3 my-auto" href="#home">
-                HOME
-              </Nav.Link>
-              <Nav.Link className="mx-3 my-auto" href="#link">
+              <Nav className="mx-3 my-auto">
+                <Link to="/home">HOME</Link>
+              </Nav>
+              <Nav.Link className="mx-3 my-auto" href="/mascotas">
                 MASCOTAS
               </Nav.Link>
               <Nav.Link className="mx-3 my-auto" href="#link">
@@ -46,23 +56,22 @@ const NavBars = () => {
                 title="INFORMACIÓN"
                 id="basic-nav-dropdown"
               >
-                <NavDropdown.Item href="#">DONACIONES</NavDropdown.Item>
-                <NavDropdown.Item href="#">ALIADOS</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">
+                  DONACIONES
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">ALIADOS</NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link className="mx-3 my-auto">
-                <Link to="/contactanos">CONTACTANOS</Link>
-              </Nav.Link>
+              <Link to="/contactanos">CONTACTANOS</Link>
               {
                 // console.log(user.photoURL)
                 <Link to="/profile">
-                  <ImgProfile src={user.photoURL} alt="" />
+                  <ImgProfile onClick={ver()} src={user.photoURL} alt="" />
                 </Link>
               }
+
               <Nav.Link className=" my-auto" href="#link">
                 {" "}
-                <ButtonLogout onClick={() => dispatch(actionLogoutAsync())}>
-                  LOGOUT
-                </ButtonLogout>
+                <ButtonLogout onClick={onClick}>LOGOUT</ButtonLogout>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
@@ -70,67 +79,6 @@ const NavBars = () => {
       </Navbar>
     </div>
   );
-    const dispatch = useDispatch();
-
-    const onClick =()=>{
-        dispatch(actionLogoutAsync())
-        localStorage.clear();
-        document.location.reload(true);
-    }
-
-
-    const user = useSelector(store => store.loginStore)
-    console.log(user)
-
-    const ver = (e) => {
-        
-       
-
-    }
-    return (
-        <div>
-            <Navbar className='m-0' expand="lg">
-                <Container>
-                    <Navbar.Brand >
-                        <Link to='/home'>
-                            <img
-                                src={logo}
-                                width="124"
-                                height="124"
-                                className="d-inline-block align-top"
-                                alt="React Bootstrap logo"
-                            />
-                        </Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
-                        <Nav className="justify-content-end">
-
-
-                            <Nav className='mx-3 my-auto' ><Link to='/home'>HOME</Link></Nav>
-                            <Nav.Link className='mx-3 my-auto' href="/mascotas">MASCOTAS</Nav.Link>
-                            <Nav.Link className='mx-3 my-auto' href="#link">ADOPCIÓN</Nav.Link>
-
-                            <NavDropdown className='mx-3 my-auto' title="INFORMACIÓN" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">DONACIONES</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">ALIADOS</NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link className='mx-3 my-auto' href="#link">CONTACTANOS</Nav.Link>
-                            {
-                                // console.log(user.photoURL)
-                                <Link to='/profile'><ImgProfile onClick={ver()} src={user.photoURL} alt="" /></Link>
-
-
-                            }
-
-                            <Nav.Link className=' my-auto' href="#link"> <ButtonLogout onClick={onClick}>LOGOUT</ButtonLogout></Nav.Link>
-
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    );
 };
 
 export default NavBars;
